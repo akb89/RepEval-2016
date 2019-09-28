9ca13c27952979e1e2917eba0c914d231807bb9d
AKB <akb@3azouz.net>
2019-09-28 19:12:14 +0200

    fix

--- run_repeval_multi.sh
+++ run_repeval_multi.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
-home=$(pwd)/keras
-dataPath=$home'/ner/data/ner'
+
 FILES=$(find $1 -type f -name '*.bin' -o -name '*.npy')
-DATASETS=($dataPath/CoNLL03 $dataPath/CoNLL00 $dataPath/PTB-pos)
 OUT=$2
 JOBS=10
 
 runner() {
+	home=$(pwd)/keras
+	dataPath=$home'/ner/data/ner'
+	DATASETS=($dataPath/CoNLL03 $dataPath/CoNLL00 $dataPath/PTB-pos)
     echo /home/kabbach/venv2/bin/python $home/ner/run_xp_single.py ${DATASETS[@]} $1
 }
 export -f runner
-parallel -j ${JOBS} runner ::: ${FILES} > ${OUT} 
+parallel --citation -j ${JOBS} runner ::: ${FILES} > ${OUT} 
