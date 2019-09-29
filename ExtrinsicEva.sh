07afaaaadd46032abcb5995e2a6362724a29b67f
AKB <akb@3azouz.net>
2019-09-29 14:19:03 +0200

    fix

--- ExtrinsicEva.sh
+++ ExtrinsicEva.sh
@@ -4,21 +4,24 @@
 
 home=$(pwd)/keras
 dataPath=$home'/ner/data/ner'
+OUT=$2
 # echo $home
 
 #DIR=$home'/ner/evaluation' #"$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #echo $DIR
 FILES=$(find $1 -type f -name '*.bin' -o -name '*.npy')
 DATASETS=($dataPath/CoNLL03 $dataPath/CoNLL00 $dataPath/PTB-pos)
-/home/kabbach/venv2/bin/python $home/ner/run_xp.py ${DATASETS[@]} $FILES
-# for file in $(echo $FILES | xargs -n1 | sort | xargs)
-# do
-# 	for f in $dataPath/CoNLL03 $dataPath/CoNLL00 $dataPath/PTB-pos
-# 	do
-# 	/home/kabbach/venv2/bin/python $home/ner/mlp.py $f $file
-# 	done
-# done
-# /home/kabbach/venv2/bin/python $home/ner/output_results.py
-#
-# # result located in log and prediction file
+#/Users/akb/Desktop/venv2/bin/python $home/ner/run_xp.py ${DATASETS[@]} $FILES
+echo -e "MODEL\tCONLL00\tCONLL03\tPTB\tCONLL00-OOV\tCONLL03-OOV\tPTB-OOV" > ${OUT}
+for file in $(echo $FILES | xargs -n1 | sort | xargs)
+do
+  /home/kabbach/venv2/bin/python $home/ner/run_xp_single.py ${DATASETS[@]} $file 2>> ${OUT}
+	# for f in $dataPath/CoNLL03 $dataPath/CoNLL00 $dataPath/PTB-pos
+	# do
+	# /Users/akb/Desktop/venv2/bin/python $home/ner/mlp.py $f $file
+	# done
+done
+# /Users/akb/Desktop/venv2/bin/python $home/ner/output_results.py
+
+# result located in log and prediction file
 # wait
