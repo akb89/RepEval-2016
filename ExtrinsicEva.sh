54e71f4474f410a309fd1219d02c22a6148bfc56
AKB <akb@3azouz.net>
2019-10-05 13:44:29 +0200

    Removed ptb-pos from script

--- ExtrinsicEva.sh
+++ ExtrinsicEva.sh
@@ -10,12 +10,13 @@ OUT=$2
 #DIR=$home'/ner/evaluation' #"$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #echo $DIR
 FILES=$(find $1 -type f -name '*.bin' -o -name '*.npy')
-DATASETS=($dataPath/CoNLL03 $dataPath/CoNLL00 $dataPath/PTB-pos)
+#DATASETS=($dataPath/CoNLL03 $dataPath/CoNLL00 $dataPath/PTB-pos)
+DATASETS=($dataPath/CoNLL03 $dataPath/CoNLL00)
 #/Users/akb/Desktop/venv2/bin/python $home/ner/run_xp.py ${DATASETS[@]} $FILES
 echo -e "MODEL\tCONLL00\tCONLL03\tPTB\tCONLL00-OOV\tCONLL03-OOV\tPTB-OOV" > ${OUT}
 for file in $(echo $FILES | xargs -n1 | sort | xargs)
 do
-  /home/kabbach/venv2/bin/python $home/ner/run_xp_single.py ${DATASETS[@]} $file 2>> ${OUT}
+  /Users/akb/Desktop/venv2/bin/python $home/ner/run_xp_single.py ${DATASETS[@]} $file 2>> ${OUT}
 	# for f in $dataPath/CoNLL03 $dataPath/CoNLL00 $dataPath/PTB-pos
 	# do
 	# /Users/akb/Desktop/venv2/bin/python $home/ner/mlp.py $f $file
